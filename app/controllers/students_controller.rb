5a7b915e4f4e31b170e66f2fbfbbebd486125e3f
JonCabot <jfc0053@gmail.com>
2022-05-23 17:11:38 -0500

    commit tree 7432e93eeab0e3c2ffadbd52ed7f568d58fbb6e1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,19 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+
+    # student = Student.find_by(first_name: params[:first_name])
+    # render json: student
+
+    # student = Student.find_by(last_name: params[:last_name])
+    # render json:student
+
+    
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student 
   end
 
 end
